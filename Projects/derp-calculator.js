de6f94a2c0d4e2dff7a6a680e80f85348337a7e0
ajsya <52182942+ajsya@users.noreply.github.com>
2020-02-10 21:25:30 -0500

    Update derp-calculator.js

--- Projects/derp-calculator.js
+++ Projects/derp-calculator.js
@@ -1,7 +1,7 @@
-function myFunction() {
+function add() {
   var number1 = parseInt(document.getElementById("number1").value);
   var number2 = parseInt(document.getElementById("number2").value);
-  if (number1 === NaN || number2 === NaN) {
+  if (number1 === "NaN" || number2 === "NaN") {
     document.getElementById("answer").innerHTML = "You must be Ben, you forgot to enter a number!"
   }
   else {
@@ -16,5 +16,47 @@ function myFunction() {
       var output = (number + number2);
     }
     document.getElementById("answer").innerHTML = "The answer to " + number1 + " plus " + number2 + " is, " + output + " in Derp Math."; 
+  }
+}
+
+function subtract() {
+  var number1 = parseInt(document.getElementById("number1").value);
+  var number2 = parseInt(document.getElementById("number2").value);
+  if (number1 === "NaN" || number2 === "NaN") {
+    document.getElementById("answer").innerHTML = "You must be Ben, you forgot to enter a number!"
+  }
+  else {
+    var number = (number1 - number2);
+    if (number1 < number2) {
+      var output = (number - number1);
+    }
+    if (number2 < number1) {
+      var output = (number - number2);
+    }
+    if (number1 == number2) {
+      var output = (number - number2);
+    }
+    document.getElementById("answer").innerHTML = "The answer to " + number1 + " minus " + number2 + " is, " + output + " in Derp Math.";
+  }
+}
+
+function multiply() {
+  var number1 = parseInt(document.getElementById("number1").value);
+  var number2 = parseInt(document.getElementById("number2").value);
+  if (number1 === "NaN" || number2 === "NaN") {
+    document.getElementById("answer").innerHTML = "You must be Ben, you forgot to enter a number!"
+  }
+  else {
+    var number = (number1 * number2);
+    if (number1 < number2) {
+      var output = (number * number1);
+    }
+    if (number2 < number1) {
+      var output = (number * number2);
+    }
+    if (number1 == number2) {
+      var output = (number * number2);
+    }
+    document.getElementById("answer").innerHTML = "The answer to " + number1 + " times " + number2 + " is, " + output + " in Derp Math."; 
   }
 }
