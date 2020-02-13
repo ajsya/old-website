17afc1a04d42a9f49b0e56a6a2983bf6c09be37c
ajsya <52182942+ajsya@users.noreply.github.com>
2020-02-12 19:30:36 -0500

    Update derp-calculator.js

--- Projects/derp-calculator.js
+++ Projects/derp-calculator.js
@@ -60,3 +60,24 @@ function multiply() {
     document.getElementById("answer").innerHTML = "The answer to " + number1 + " times " + number2 + " is, " + output + " in Derp Math."; 
   }
 }
+
+function divide() {
+  var number1 = parseInt(document.getElementById("number1").value);
+  var number2 = parseInt(document.getElementById("number2").value);
+  if (number1 === "NaN" || number2 === "NaN") {
+    document.getElementById("answer").innerHTML = "You must be Ben, you forgot to enter a number!"
+  }
+  else {
+    var number = (number1 / number2);
+    if (number1 < number2) {
+      var output = (number / number1);
+    }
+    if (number2 < number1) {
+      var output = (number / number2);
+    }
+    if (number1 == number2) {
+      var output = (number / number2);
+    }
+    document.getElementById("answer").innerHTML = "The answer to " + number1 + " divided by " + number2 + " is, " + output + " in Derp Math."; 
+  }
+}
